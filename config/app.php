a3c83ec4365a0c1b69ee3d4d1de0791def8100c7
M. Imam Firdaus <mimamfirdaus5@gmail.com>
2025-06-02 22:49:07 +0700

    production bug

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'https://kelompok1a.cloudptk.my.id'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://kelompok1a.cloudptk.my.id'),
 
     /*
     |--------------------------------------------------------------------------
